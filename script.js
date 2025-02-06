eb638d84692c7944ccedead254a873aceb15ce04
01jake <162063129+01jake@users.noreply.github.com>
2025-02-06 13:58:40 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,6 @@
-document.getElementById('search-button').addEventListener('click', function() {
-    var nav = document.getElementById('main-nav');
-    var searchInput = document.getElementById('search-input');
-    var searchButton = document.getElementById('search-button');
-    var closeSearch = document.getElementById('close-search');
 
-    if (searchInput.style.display === 'none') {
-        nav.style.display = 'none';
-        searchInput.style.display = 'block';
-        closeSearch.style.display = 'block';
-        searchButton.style.display = 'none';
-    } else {
-        // Aquí puedes agregar la lógica para realizar la búsqueda
-        console.log('Buscando:', searchInput.value);
-    }
-});
 
-document.getElementById('close-search').addEventListener('click', function() {
-    var nav = document.getElementById('main-nav');
-    var searchInput = document.getElementById('search-input');
-    var searchButton = document.getElementById('search-button');
-    var closeSearch = document.getElementById('close-search');
 
-    nav.style.display = 'flex';
-    searchInput.style.display = 'none';
-    closeSearch.style.display = 'none';
-    searchButton.style.display = 'block';
-});
-
-document.getElementById('menu-button').addEventListener('click', function() {
-    var nav = document.getElementById('main-nav');
-    if (nav.style.display === 'none' || nav.style.display === '') {
-        nav.style.display = 'block';
-    } else {
-        nav.style.display = 'none';
-    }
-});
 
 let lastScrollTop = 0;
 window.addEventListener('scroll', function() {
@@ -51,4 +17,26 @@ window.addEventListener('scroll', function() {
     }
 
     lastScrollTop = scrollPosition <= 0 ? 0 : scrollPosition; // Para evitar valores negativos
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita el envío del formulario por defecto
+
+    var nombre = document.getElementById('nombre').value.trim();
+    var apellido = document.getElementById('apellido').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var direccion = document.getElementById('direccion').value.trim();
+    var telefono = document.getElementById('telefono').value.trim();
+    var servicio = document.getElementById('servicio').value.trim();
+    var mensaje = document.getElementById('mensaje').value.trim();
+
+    if (!nombre || !apellido || !email || !direccion ||  !telefono) {
+        alert('Por favor, complete todos los campos del formulario.');
+        return;
+    }
+
+    var whatsappMessage = `Nombre: ${nombre}%0AEmail: ${email}%0ATeléfono: ${telefono}%0AServicio: ${servicio}%0AMensaje: ${mensaje} %0ADireccion: ${direccion } %0AApellido: ${apellido}`;
+    var whatsappURL = `https://wa.me/526623645921?text=${whatsappMessage}`; // Reemplaza con tu número de WhatsApp
+
+    window.open(whatsappURL, '_blank'); // Abre el enlace de WhatsApp en una nueva pestaña
 });
